f54d21c0a87354a2f02ae950189b22af078eee31
AverageGatsby98 <roses20@up.edu>
2019-02-21 22:33:46 -0800

    Initial commit

---
+++ app/src/main/java/cs301/up/edu/labyrinthhw4d/MainActivity.java
@@ -0,0 +1,17 @@
+package cs301.up.edu.labyrinthhw4d;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author Spencer Nelson, Philip Robinson, Spencer Rose, Erik Torkelson
+ */
+public class MainActivity extends AppCompatActivity {
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
